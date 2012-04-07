381087e724a0b9a6979d4eceb57d1b16c88f9455
John Costa <john.costa@gmail.com>
2012-04-07 08:33:16 -0400

    change __init__ back to file, look for currentenv

--- readthedocs/settings/__init__.py
+++ readthedocs/settings/__init__.py
@@ -1,1 +1,10 @@
-postgres.py
+try:
+    from .currentenv import *
+except ImportError:
+    import sys
+    msg = ("Error importing settings/currentenv.py. Did you forget to symlink "
+           "your local settings?\n")
+    sys.stderr.write(msg)
+
+    import traceback
+    sys.stderr.write(traceback.format_exc())
