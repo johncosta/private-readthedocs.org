2d85238f38f51f2211517c63f3ad2724eb72577f
John Costa <john.costa@gmail.com>
2012-04-09 07:01:17 -0400

    management command used for one button setup

---
+++ readthedocs/core/management/commands/create_api_user.py
@@ -0,0 +1,27 @@
+import logging
+from django.contrib.auth.models import User
+
+from django.core.management.base import BaseCommand
+from projects import tasks
+from projects.models import Project, ImportedFile
+from builds.models import Version
+
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+
+    help = '''\
+Creates a readthedocs demo api user for one button deploy
+'''
+
+    def handle(self, *args, **kwargs):
+        """ Creates demo api user """
+        try:
+            user = User.objects.create(username="test", email="test@example.com")
+            user.set_password("test")
+            user.save()
+        except Exception, e:
+            log.error(e)
+            log.error("Unable to configure test user.. you may need to do this manually")
+
+
